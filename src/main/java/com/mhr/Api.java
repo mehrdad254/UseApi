15b773ba2a73aa05ae22b8f175ed98dc3d1e7bf9
mehrdad <mehrdad@localhost>
2020-09-13 14:20:13 +0430

    finish project

---
+++ src/main/java/com/mhr/Api.java
@@ -0,0 +1,28 @@
+package com.mhr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.json.JSONObject;
+import javax.net.ssl.HttpsURLConnection;
+
+public class Api {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		System.out.println(getDog());
+
+	}
+
+	public static String getDog() throws MalformedURLException, IOException{
+        String httpsURL = "https://dog.ceo/api/breeds/image/random";
+        URL myUrl = new URL(httpsURL);
+        HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        JSONObject json = new JSONObject(in.readLine());
+        String Picture = json.getString("message");
+        return Picture;
+    }
+}
